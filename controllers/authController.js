36ffc81b6c22ef34b7f6a3b6819dbb22d84c9858
abdulwahab <abdulwahabdev0@gmail.com>
2025-06-15 11:02:15 +0500

    set the domain for cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -94,7 +94,7 @@ exports.login = catchAsync(async (req, res, next) => {
     serialize("authorization", `Bearer ${token}`, {
       httpOnly: true,
       maxAge: 60 * 60 * 24 * 7,
-      path: "/",
+      domain: ".flexisaves.com",
     })
   );
 
@@ -185,7 +185,7 @@ exports.googleAuthCallback = catchAsync(async (req, res) => {
     serialize("authorization", `Bearer ${token}`, {
       httpOnly: true,
       maxAge: 60 * 60 * 24 * 7,
-      path: "/",
+      domain: ".flexisaves.com",
     })
   );
 
@@ -593,7 +593,7 @@ exports.userLogout = catchAsync(async (req, res) => {
     serialize("authorization", ``, {
       httpOnly: true,
       maxAge: 0,
-      path: "/",
+      domain: ".flexisaves.com",
     })
   );
   res.status(200).json({
